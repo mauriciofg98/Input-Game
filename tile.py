005fc33b5c9070b7b9f6e6487df3a5f18b97c1dd
DESKTOP-NMBIIAH\Roberto <roberri898@gmail.com>
2019-11-04 23:10:11 -0600

    Added self.

--- tile.py
+++ tile.py
@@ -10,18 +10,18 @@ class Tile:
             self.path[i+1]=tilepath[i]
         self.belongs=player
     
-    def move():
+    def move(self):
         # places tile in board
-        OnPath=OnPath+1
-        if(OnPath>4):
-            OnPath=1
-        return path[OnPath]
+        self.OnPath=self.OnPath+1
+        if(self.OnPath>4):
+            self.OnPath=1
+        return self.path[OnPath]
 
     def currentlocation():
-        return path[OnPath]
+        return self.path[self.OnPath]
 
-    def end():
-        if OnPath== 4:
+    def end(self):
+        if self.OnPath== 4:
             return True
         else
             return False
