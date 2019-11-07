009420f57b256e48878c742eafcab3ad88782ecb
mauriciofg98 <mauricio@Mauricios-MBP-2.rgv.rr.com>
2019-11-06 19:33:35 -0600

    pygame

--- board.py
+++ board.py
@@ -1,23 +1,35 @@
 from player import Player
 import pygame
 
-# player1 = Player("R")
-# player2 = Player("B")
-# pla
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+pygame.display.set_caption("Input")
+x=50
+y=50
+width = 40
+height = 60
+vel =5
+
+run = True
+
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    win.fill(WHITE)
+
+    pygame.draw.circle(win, BLACK, [80, 80], 80, 0)
+    pygame.display.flip()
+
+pygame.quit()
+
+player1 = Player("R")
+player2 = Player("B")
+
+
 #Players=[Player("R"),Player("B")]
-
-play1= Player("R")
-play1.printRestTiles()
-play1.addToReady(5)
-play1.printRestTiles()
-
-play1.addToReady(4)
-
-play1.playPiece()
-play1.printRestTiles()
-
-play1.printBoardTiles()
-play1.printReadyTiles()
-
-
-play1.loc()
